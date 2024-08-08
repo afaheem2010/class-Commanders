b5f4f58c7228ab56103bc41e7f6c2e4195a040b3
Ahmad <afaheem2010@gmail.com>
2024-08-07 21:09:10 -0400

    initial push

---
+++ src/test/tekSchool.java
@@ -0,0 +1,17 @@
+package test;
+
+public class tekSchool {
+
+    public static void main(String[] args) {
+
+       int num1= 14;
+       int num2= 45;
+
+       int num3= num1+ num2;
+
+
+
+        System.out.println("My name is ahmad");
+        System.out.println(num3);
+    }
+}
